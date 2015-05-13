dec2bf1ca50ce1bb990ac0ecae37afafede167de
Daniil Shevelev <daniil.shevelev@gmail.com>
2015-05-12 20:54:48 -0400

    Filed file access code by handling access of file inside the jar.

--- src/main/java/com/niewlabs/careerpath/HelperRW.java
+++ src/main/java/com/niewlabs/careerpath/HelperRW.java
@@ -5,12 +5,13 @@ import com.google.gson.reflect.TypeToken;
 
 import java.io.IOException;
 import java.lang.reflect.Type;
+import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.nio.file.*;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by dash on 5/12/2015.
@@ -25,12 +26,28 @@ public class HelperRW {
         Type ourMap = new TypeToken<HashMap<String, List<String>>>() {
         }.getType();
         String codes_to_titles = null;
+
+        FileSystem fs = null;
         try {
-            codes_to_titles = new String(Files.readAllBytes(Paths.get(fileUrl.toURI())), "UTF-8");
+            URI uri = fileUrl.toURI();
+
+            final Map<String, String> env = new HashMap<>();
+            final String[] array = uri.toString().split("!");
+            fs = FileSystems.newFileSystem(URI.create(array[0]), env);
+            final Path path = fs.getPath(array[1]);
+
+            codes_to_titles = new String(Files.readAllBytes(path), "UTF-8");
         } catch (IOException e) {
             e.printStackTrace();
         } catch (URISyntaxException e) {
             e.printStackTrace();
+        } finally {
+            // FIXME: Clean this up...Oh java...
+            try {
+                fs.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
         HashMap<String, List<String>> jsonData = gson.fromJson(codes_to_titles, ourMap);
         return jsonData;
